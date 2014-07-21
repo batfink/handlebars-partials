8dd9d03b5a17275133a54eff63bc9924bfd59137
Victor Nystad <vnystad@gmail.com>
2014-07-21 13:31:00 +0200

    fixed filepath and dependencies

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var fs = require('fs');
 var path = require('path');
 
 function partials(handlebars) {
-    var partials = fs.readdirSync('partials');
+    var partials = fs.readdirSync(__dirname + '/partials');
 
     partials.forEach(function(partial) {
-        handlebars.registerPartial(path.basename(partial, '.hbs'), partial);
+        handlebars.registerPartial(path.basename(partial, '.hbs'), fs.readFileSync(__dirname + '/partials/' + partial).toString());
     })
 
     return handlebars;
